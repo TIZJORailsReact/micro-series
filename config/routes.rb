a5c8dde07e83e15c74908b7831dcdfc3c2b6d472
Mateusz Sędkowski <mateusz.sedkowski@htdevelopers.com>
2018-12-04 18:09:23 +0100

    Correct routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   apipie
-  resources :series, only: %i(index show) do
-    resources :seasons, only: :index
+  resources :series, only: %i(index show), :defaults => { :format => 'json' } do
+    resources :seasons, only: :index, :defaults => { :format => 'json' }
   end
 
-  resources :seasons, only: :show do
-    resources :episodes, only: :index
+  resources :seasons, only: :show, :defaults => { :format => 'json' } do
+    resources :episodes, only: :index, :defaults => { :format => 'json' }
   end
 
-  resources :episodes, only: :show
+  resources :episodes, only: :show, :defaults => { :format => 'json' }
 end
