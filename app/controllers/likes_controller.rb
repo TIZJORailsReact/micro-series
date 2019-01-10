8da1193e0c90cf9a6853a56bccb8c92eb23c005b
Mateusz Sędkowski <mateusz.sedkowski@htdevelopers.com>
2019-01-10 01:29:47 +0100

    Code refactor.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,31 +1,28 @@
 class LikesController < ApplicationController
-  class LikesController < ApplicationController
+  def create
+    @like = Series.find(params[:series_id]).likes.new(likes_params)
 
-    def create
-      @like = Series.find(params[:series_id]).likes.new(likes_params)
+    if @like.save!
+      head 200
+    else
+      head 422
+    end
+  end
 
-      if @like.save!
+  def destroy
+    @like = Series.find(params[:series_id]).likes.find_by(user: params[:login])
+    if @like.present?
+      if @like.destroy
         head 200
       else
         head 422
       end
+    else
+      head 404
     end
+  end
 
-    def destroy
-      @like = Series.find(params[:series_id]).likes.find_by(user: params[:login])
-      if @like.present?
-        if @like.destroy
-          head 200
-        else
-          head 422
-        end
-      else
-        head 404
-      end
-    end
-
-    def likes_params
-      params.require(:like).permit(:login)
-    end
+  def likes_params
+    params.require(:like).permit(:login)
   end
 end
